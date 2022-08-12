a439ab6d800940d401800e222b18aaf9fa76e856
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-12 12:21:04 +0000

    configuring base

---
+++ Classes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from flet import app,Page,Text,ElevatedButton
+
+
+class StartPage:
+
+
+    def configureControls(self):
+
+    def createControls(self):
+        
+
+    def __init__(self,page):
+        self.page = page
