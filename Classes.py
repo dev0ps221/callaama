916859af8fa497e3d45e1e80d3ff6b17df49cc3a
dev0ps221 <seybaxscorpion@gmail.com>
2022-08-12 12:24:40 +0000

    configuring base

--- Classes.py
+++ Classes.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
-from flet import app,Page,Text,ElevatedButton
-
+from flet import app,Page,Text,ElevatedButton,Row,Column
+app_title = 'Callaama'
+app_version = '0.1'
 
 class StartPage:
-
+    starttext_container = None
+    startbutton_container = None
+    starttext = Text(value=f'{app_title} V{app_version}')
+    startbutton = ElevatedButton(text='COMMENCER')
 
     def configureControls(self):
+        self.starttext_container.controls.append(self.starttext)
+        self.startbutton_container.controls.append(self.starttext)
+
+    def showControls(self):
+        self.page.add(self.starttext_container)
+        self.page.add(self.startbutton_container)
+    
+    def update(self):
+        self.page.update()
 
     def createControls(self):
-        
+        self.starttext_container = Column()
+        self.startbutton_container = Column()
 
     def __init__(self,page):
         self.page = page
